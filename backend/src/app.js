3ab58fdb16e3ac86a8805c713d06d65f61307fc7
Bhrighu12 <bhrighugupta@gmail.com>
2024-09-13 20:41:09 +0530

    First Commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,7 @@ import {Server} from "socket.io";
 import mongoose from "mongoose";
 
 import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
 
 import { connectToSocket } from "./controllers/socketManager.js";
 
@@ -14,20 +15,20 @@ const server = createServer(app);
 const io = connectToSocket(server);
 
 app.set("port",(process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({limit:"40kb", extended:true}));
 
-app.get("/home",(req,res)=>{
-    return res.json({"hello":"World"})
-});
+app.use("/api/v1/users", userRoutes);
 
 
 const start = async() => {
     app.set("mongo_user")
-        const connectionDb = await mongoose.connect("")
+        const connectionDb = await mongoose.connect("mongodb+srv://bhrighugupta:8hdi5nRoLH6QiHTL@cluster0.hcebt.mongodb.net/")
         console.log(`MONGO CONNECTED DB HOST: ${connectionDb.connection.host}`)
         server.listen(app.get("port"),()=>{
         console.log("LISTEN ON PORT 8000")
     });
-
 
 }
 
---
+++ frontend/my-app/src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+// import { Link, useNavigate } from 'react-router-dom'
+
+export default function HomePage() {
+
+    // const router = useNavigate();
+
+    return(
+        <div className="home">
+            <h1>GenAi</h1>
+        </div>
+    )
+}
